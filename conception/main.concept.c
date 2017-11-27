cc5f9fda925d8926a4f3a2372f2f2937cfe42e00
Unelith <unelithally333@gmail.com>
2017-11-27 08:47:50 +0100

    Create main.concept.c
    Don't compile this file, this is not working code!

---
+++ conception/main.concept.c
@@ -0,0 +1,35 @@
+/* DON'T COMPILE THIS, THIS IS JUST A GENERAL CONCEPTION */
+
+#include <stdio.h>
+// other necessary includes
+#include "display.h"
+#include "mapio.h"
+#include "control.h"
+
+int main(int argc, char **argv){
+  char **map; //2-d array of size defined later with malloc()
+  int rows, columns; // Map dimensions
+  int crnt_player; // The current player index
+  int max_players; // Maximum amount of players
+  int max_pngns; // Amount of penguins per player
+  int scores[6]; // The score for each player
+  int ***pngns_pos; //3-d array containing positions of the penguins
+  //pngns_pos[player id][penguin id][0 or 1 stand for x or y]
+
+  // File: mapio.c
+  // A function that will fill the variables according to the data
+  // found in the input file.
+  read_data(argv[1], map, &crnt_player, &max_players, etc...);
+
+  // File: control.c
+  // Wait for the player input, inform the player if the input was incorrect, etc.,
+  // then alter the variables accordingly.
+  // The print_map() function from the display.c file would be called inside of
+  // the player_input function:
+  // print_map(map, rows, columns)
+  player_input(map, rows, columns, &crnt_player, etc...);
+
+  // File: mapio.c
+  // Output the variables into a file
+  write_data(argv[2], map, rows, columns, crnt_player, etc...);
+}
