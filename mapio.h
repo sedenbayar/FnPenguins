40a49c083576711acc5cb264d97b7820043109f8
Unelith <unelithally333@gmail.com>
2017-11-28 02:48:46 +0100

    Update mapio.h
    Adding and assigning the two main functions

--- mapio.h
+++ mapio.h
@@ -1,6 +1,20 @@
+////////////////////
+// mapio.h        //
+// by David & Can //
+////////////////////
 #ifndef MAPIO_H_INCLUDED
 #define MAPIO_H_INCLUDED
 
+#include "datastrc.h"
 
+///////////
+// David //
+///////////
+int read_data(char* ifilename, struct Gmdt *gmdt);
+
+/////////
+// Can //
+/////////
+int write_data(char* ofilename, struct Gmdt *gmdt);
 
 #endif // MAPIO_H_INCLUDED
