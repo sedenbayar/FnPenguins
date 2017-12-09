c4ef7fec6d5f76ea7848d4a04bd77594460a457d
Unelith <unelithally333@gmail.com>
2017-12-09 19:15:50 +0100

    Changing some error conditions and return values
    I don't know why max_players limit was 5 before, a silly mistake on my part.

--- mapio.c
+++ mapio.c
@@ -48,7 +48,8 @@ int read_data(char* ifilename, struct Gmdt *gmdt){
         else if (n == 1){
             if(l == 0) {
                 gmdt->max_players = c - 48;
-                if(gmdt->max_players > 5) return 2;
+                if(gmdt->max_players > 6) return 2;
+                else if(gmdt->max_players < 2) return 3;
             }
         }
         // Third line
@@ -62,7 +63,7 @@ int read_data(char* ifilename, struct Gmdt *gmdt){
             if(l == 0){
                 if (c == 'p') gmdt->phase = 1;
                 else if (c == 'm') gmdt->phase = 2;
-                else return 3;
+                else return 4;
             }
         }
 
