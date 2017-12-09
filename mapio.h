d79fc9498f8f2272a7f7dbc90098eb45537792c2
Unelith <unelithally333@gmail.com>
2017-12-09 19:21:57 +0100

    Update mapio.h

--- mapio.h
+++ mapio.h
@@ -17,7 +17,11 @@ int is_pos_correct(char c);
 /* The return value from this function will represent
    the success (if there were any errors, etc.).
    It will return 0 on success, and other values when
-   encountering specific errors.                      */
+   encountering specific errors regarding the file:
+   1 - current player index is larger than 5
+   2 - the max amount of players is greater than 6
+   3 - the max amount of player is smaller than 2
+   4 - unknown phase name given                      */
 int read_data(char* ifilename, struct Gmdt *gmdt);
 
 /////////
