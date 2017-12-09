771e332b5547390ca528e3e7f3a7f430ba23a888
Unelith <unelithally333@gmail.com>
2017-12-09 19:04:22 +0100

    Adding functions to free memory as well

--- datastrc.c
+++ datastrc.c
@@ -21,3 +21,20 @@ void allocate_pngns(struct Gmdt *gmdt){
         for (j = 0; j < gmdt->max_pngns; j++)
             gmdt->pngns_pos[i][j] = (int*)malloc(2*sizeof(int));
 }
+
+void free_memory(struct Gmdt *gmdt){
+    int i;
+    for (i = 0; i < gmdt->columns; i++)
+        free(gmdt->map[i]);
+    free(gmdt->map);
+}
+
+void free_pngns(struct Gmdt *gmdt){
+    int i, j;
+    for (i = 0; i < gmdt->max_players; i++)
+        for (j = 0; j < gmdt->max_pngns; j++)
+            free(gmdt->pngns_pos[i][j]);
+    for (i = 0; i < gmdt->max_players; i++)
+        free(gmdt->pngns_pos[i]);
+    free(gmdt->pngns_pos);
+}
