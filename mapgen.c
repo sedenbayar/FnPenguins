32e411f973ede4e98af447bd5dfcf1fe483f7678
GaoningGao <33690279+GaoningGao@users.noreply.github.com>
2017-12-09 01:14:55 +0100

    Update mapgen.c

--- mapgen.c
+++ mapgen.c
@@ -1,1 +1,32 @@
 #include "mapgen.h"
+#include "datastrc.h"
+
+
+
+int random_number(int min, int max){
+
+    return (rand()%(max-min+1)+min);
+
+}
+
+void generate_map(struct Gmdt *gmdt){
+    int i,j;
+    int SIZE;
+    
+    SIZE=random_number(15,25);
+    gmdt->rows=SIZE;
+    gmdt->columns=SIZE;
+    for(i=0;i<SIZE;i++){
+        for(j=0;j<SIZE;j++){
+           gmdt->map[i][j]=random_number(0,3);
+            if(gmdt->map[i][j]==0)
+            gmdt->map[i][j]=' ';
+         
+        }
+    
+    
+    }
+
+
+
+}
