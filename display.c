2b218f53abbcbba9b49c12c9ad4c7a9e6ce8dd84
Unelith <unelithally333@gmail.com>
2017-12-11 13:08:00 +0100

    Update display.c

--- display.c
+++ display.c
@@ -9,13 +9,26 @@
 #include "display.h"
 
 void print_map(char **data, int rows, int columns){
+    printf("\n\n");
     int i,j;
+    printf("     ");
+    for(j=0;j<columns;j++){
+        if(!(j/10))
+            printf("%d  ",j);
+        else
+            printf("%d ",j);
+    }
+    printf("\n\n");
     for(i=0;i<rows;i++){
+        if(!(i/10))
+            printf("%d ", i);
+        else
+            printf("%d", i);
         if(!(i % 2))
-            printf(" ");
+            printf("  ");
         for(j=0;j<columns;j++){
-            printf(" %c",data[j][i]);
+            printf("  %c",data[j][i]);
         }
-        printf("\n");
+        printf("\n\n");
     }
 }
