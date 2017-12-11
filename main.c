023846d24db5607afccae6a4fb66c4ffdd2e6040
Unelith <unelithally333@gmail.com>
2017-12-11 20:44:19 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -39,6 +39,17 @@ int main(int argc, char* argv)
     // Allocate memory for the penguins
     allocate_pngns(&gmdt);
 
+    /*print_map(gmdt.map, gmdt.rows, gmdt.columns);
+    printf("Max players = %d\nMax penguins = %d\n", gmdt.max_players, gmdt.max_pngns);
+    save_file("output.txt", &gmdt);*/
+
+    /*int i, j, k;
+    for (i = 0; i < gmdt.max_players; i++)
+        for (j = 0; j < gmdt.max_pngns; j++)
+            printf("(%d, %d)\n", gmdt.pngns_pos[i][j][0], gmdt.pngns_pos[i][j][1]);*/
+
+    save_file("output.txt",&gmdt);
+
     // Player input here
     placement(gmdt.max_players, gmdt.max_pngns, gmdt.pngns_pos, gmdt.map, gmdt.rows, gmdt.columns);
     gmdt.phase = 2;
@@ -54,3 +65,4 @@ int main(int argc, char* argv)
 
     return 0;
 }
+
--- mapgen.c
+++ mapgen.c
@@ -16,22 +16,22 @@ int random_number(int min, int max){
 void generate_map(struct Gmdt *gmdt){
     int i,j;
     int SIZE;
-    
-    SIZE=random_number(15,25);
+    srand((unsigned int)time(NULL));
+    SIZE=random_number(16,22);
     gmdt->rows=SIZE;
     gmdt->columns=SIZE;
-    
+
     allocate_memory(gmdt);
-    
+
     for(i=0;i<SIZE;i++){
         for(j=0;j<SIZE;j++){
            gmdt->map[i][j]=random_number('0','3');
             if(gmdt->map[i][j]=='0')
             gmdt->map[i][j]=' ';
-         
+
         }
-    
-    
+
+
     }
 
 
