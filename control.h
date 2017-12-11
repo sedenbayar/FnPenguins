4e3f1e5131baf30161b3719f0eb5b57ba7790031
Jakub Kicek <30901320+jakubKicek@users.noreply.github.com>
2017-12-11 11:02:03 +0100

    Update control.h

--- control.h
+++ control.h
@@ -1,6 +1,10 @@
 #ifndef CONTROL_H_INCLUDED
 #define CONTROL_H_INCLUDED
 
+#include <stdbool.h>
 
+void move_penguin(int x,int y,int max_pl, int max_pg,int pngns_pos[6][2][2]);
+bool is_move_possible(int pl,int pg,int max_pl, int max_pg,int pngns_pos[6][2][2]);
+void a_turn(int max_pl,int max_pg,int pngns_pos[6][2][2]);
 
 #endif // CONTROL_H_INCLUDED
