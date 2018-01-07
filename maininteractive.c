e39bb1069be37ee7097fcc79a531f5b9bc123085
sedenbayar <33690229+sedenbayar@users.noreply.github.com>
2018-01-07 22:36:22 +0100

    Create maininteractive.c

---
+++ maininteractive.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "datastrc.h"
+#include "mapgen.h"
+#include "mapio.h"
+#include "control.h"
+#include "display.h"
+#include <time.h>
+
+void init_settings(struct Gmdt *gmdt){
+    do{
+        printf("Number of players (2-6): ");
+        scanf("%d", &gmdt->max_players);
+    }while(gmdt->max_players < 2 || gmdt->max_players > 6);
+
+    do{
+        printf("Number of penguins per player (1 or greater): ");
+        scanf("%d", &gmdt->max_pngns);
+    }while(gmdt->max_pngns < 1);
+
+    gmdt->phase = 1; // start with placement
+    gmdt->crnt_player = 0; // start from the first player
+
+    // Populate score array with 0's
+    int i;
+    for (i = 0; i < 6; i++)
+        gmdt->scores[i] = 0;
+}
+
+int main(int argc, char* argv)
+{
+    struct Gmdt gmdt;
+
+    generate_map(&gmdt);
+
+    // Initialize settings
+    init_settings(&gmdt);
+
+    allocate_memory(&gmdt);
+
+    // Allocate memory for the penguins
+    allocate_pngns(&gmdt);
+
+    // Player input here
+    placement(gmdt.max_players, gmdt.max_pngns, gmdt.pngns_pos, gmdt.map, gmdt.rows, gmdt.columns);
+    gmdt.phase = 2;
+    gmdt.crnt_player = 0;
+    while(is_move_possible(&gmdt))
+    {
+        a_turn(gmdt.max_players, gmdt.max_pngns, gmdt.pngns_pos, gmdt.map, gmdt.scores, gmdt.rows, gmdt.columns);
+    }
+    save_file(argv[2], &gmdt);
+
+    // Free memory
+    free_memory(&gmdt);
+    free_pngns(&gmdt);
+
+    return 0;
+}
