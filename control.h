1de87134f18254423fc3ea4f308e0633673450d0
Unelith <unelithally333@gmail.com>
2018-01-08 02:02:20 +0100

    Update control.h

--- control.h
+++ control.h
@@ -4,15 +4,16 @@
 #include <stdbool.h>
 #include "datastrc.h"
 
-int count_points(int x,int y,char **map,int scores[6]);
+int count_points(int x,int y,struct Gmdt *gmdt);
 void a_turn(struct Gmdt *gmdt);  //single turn for all players, 1st player moves all of his pngns, then second one etc.
 
-void upleft(int dist,struct Gmdt *gmdt,int pg);
-void left(int dist,struct Gmdt *gmdt,int pg);
-void downleft(int dist,struct Gmdt *gmdt,int pg);
-void upright(int dist,struct Gmdt *gmdt,int pg);
-void right(int dist,struct Gmdt *gmdt,int pg);
-void downright(int dist,struct Gmdt *gmdt,int pg);
-void move_penguin(struct Gmdt *gmdt,int pg);
+bool upleft(int dist,struct Gmdt *gmdt,int pg,int pl);
+bool left(int dist,struct Gmdt *gmdt,int pg,int pl);
+bool downleft(int dist,struct Gmdt *gmdt,int pg,int pl);
+bool upright(int dist,struct Gmdt *gmdt,int pg,int pl);
+bool right(int dist,struct Gmdt *gmdt,int pg,int pl);
+bool downright(int dist,struct Gmdt *gmdt,int pg,int pl);
+void move_penguin(struct Gmdt *gmdt,int pg,int pl);
+void placement(struct Gmdt *gmdt);
 
 #endif // CONTROL_H_INCLUDED
