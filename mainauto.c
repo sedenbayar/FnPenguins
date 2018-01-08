e50df1866a1cc6d4af4505659a6cf626e2cd7a89
Unelith <unelithally333@gmail.com>
2018-01-08 09:14:49 +0100

    Update mainauto.c

--- mainauto.c
+++ mainauto.c
@@ -1,45 +1,37 @@
 #include <stdio.h>
-#include "display.h"
 #include "mapio.h"
 #include "datastrc.h"
 #include "control.h"
+#include "ai.h"
+#include "time.h"
 
-int main(int argc, char *argv){
+int main(int argc, char *argv[]){
 
+srand(time(NULL));
+if(argc >= 3)
+{
+    struct Gmdt gmdt;
+    read_data(argv[1], &gmdt);
 
-if(argc == 2)
-   {
-    read_data(argv[1], *gmdt);
+    if(gmdt.phase == 1)
+    {
+        place_penguin_ai(&gmdt);
+        // update phase
+    }
 
-    place_penguin_ai(&gmdt);
-    move_player_ai(&gmdt);
+    if(gmdt.phase == 2)
+    {
+        move_player_ai(&gmdt);
+        //move_player functions update current player number
+    }
 
-   }
-   else {
-    printf("Insufficient arguments.\nUsage: program.exe inputFileName outputFileName");
-    return 1;
-   }
-
-}
-
-if(gmdt->phase == 1){
-    place_penguin(*gmdt);
-
-    //update phase
-
-    save_file(argv[2], *gmdt);
-    free_memory(struct Gmdt, *gmdt);
-    free_pngns(struct Gmdt, *gmdt);
+    save_file(argv[2], &gmdt);
+    free_memory(&gmdt);
+    free_pngns(&gmdt);
     return 0;
 }
-
-if(gmdt->phase == 2){
-    move_player(*gmdt);
-    //move_player functions update current player number
-
-
-    save_file(argv[2], *gmdt);
-    free_memory(struct Gmdt, *gmdt);
-    free_pngns(struct Gmdt, *gmdt);
-    return 0;
+    else {
+     printf("Insufficient arguments.\nUsage: program.exe inputFileName outputFileName");
+     return 1;
+    }
 }
