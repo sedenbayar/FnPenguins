154a0fd7c48c147bda36fde60303fb72f0eda786
adomosla <33690326+adomosla@users.noreply.github.com>
2018-01-22 06:47:03 +0100

    Showing colors is it your enemy or not

--- display.c
+++ display.c
@@ -1,21 +1,24 @@
-//
+////
 //  display.c
 //  board
 //
 //  Created by app on 2017/11/21.
 //  Copyright © 2017年 ning. All rights reserved.
-//
+//  with some changes Adam Domoslawski. 
+//  All rights reserved 
+//  
 
 #include "display.h"
 #include "env.h"
 
-void print_map(char **data, int rows, int columns){
+
+void print_map(char **data, int rows, int columns, int crnt_player){
     printf("\n\n");
     int i,j;
     printf("    ");
     for(j=0;j<columns;j++){
         if(!(j/10))
-            clrprintf(YELLOW,BLACK,"%d   ",j);// little difference ;)
+            clrprintf(YELLOW,BLACK,"%d   ",j);
         else
             clrprintf(YELLOW, BLACK,"%d  ",j);
     }
@@ -28,7 +31,39 @@ void print_map(char **data, int rows, int columns){
         if(!(i % 2))
             printf("  ");
         for(j=0;j<columns;j++){
-            printf("%c   ",data[j][i]);
+            int plxr ;
+            for ( plxr=1; plxr<=6; plxr++)
+            {
+                if(plxr==crnt_player)
+                {
+                    if((data[j][i]!=96+crnt_player)&&(data[j][i]!=84+crnt_player)&&
+                       (data[j][i]!=64+crnt_player)&&(data[j][i]!='2')&&(data[j][i]!='1')&&(data[j][i]!='3'))
+                   {
+
+                        clrprintf(RED, BLACK,"%c   ",data[j][i]);
+                       break;
+                   }
+                    else if ((data[j][i]==96+crnt_player)||(data[j][i]==84+crnt_player)||
+                       (data[j][i]==64+crnt_player))
+                       {
+                      clrprintf(BLUE, BLACK,"%c   ",data[j][i]);
+                      break;
+                       }
+                    else
+                    {
+                        printf("%c   ",data[j][i]);
+                       break;
+
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+
+            }
+
+
         }
         printf("\n\n");
     }
